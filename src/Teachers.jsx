e2b80e6320015639968c26b5142843b73ee54b2b
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-03-26 20:57:12 +0430

    another new codes

---
+++ src/Teachers.jsx
@@ -0,0 +1,6 @@
+export default function Teachers() {
+     return (
+        <h1>Teachers List</h1>
+       
+     )
+} 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Teachers from './Teachers.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Teachers/>
   </StrictMode>,
 )
