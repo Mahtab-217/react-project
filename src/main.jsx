f524df077fa4c86d1b3e787ccf2d1e39490fcf23
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-03-27 20:47:06 +0430

    new info about teachers

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Teachers from './Teachers.jsx'
+ const teachersList=[
+  {
+    id:1,
+    name:"Muhammad",
+    lastName: "Qasimi",
+    age:27,
+    phone:"0798675643",
+  }
+ ]
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
