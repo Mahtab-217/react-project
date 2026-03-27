c07ca3da2c06ff6cf72ffc49939b6bfcd7889741
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-03-27 20:51:15 +0430

    updated codes

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,28 @@ import Teachers from './Teachers.jsx'
     lastName: "Qasimi",
     age:27,
     phone:"0798675643",
-  }
+  },
+  {
+    id:2,
+    name:"Muhammad Aman",
+    lastName: "Yawari",
+    age:17,
+    phone:"0798484843",
+  },
+  {
+    id:3,
+    name:"Muhammad Qasim",
+    lastName: "Madad Zada",
+    age:23,
+    phone:"07984847382",
+  },
+  {
+    id:4,
+    name:"Ali",
+    lastName: "Gholami",
+    age:28,
+    phone:"0798690784",
+  },
  ]
 
 
